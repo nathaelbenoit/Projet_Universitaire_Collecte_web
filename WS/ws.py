55aea3a27cc012cf6efead86d96cbd98363f4e5e
butsdcamillecarraco <camille_carraco@outlook.fr>
2025-11-06 12:13:52 +0100

    Update ws.py

--- WS/ws.py
+++ WS/ws.py
@@ -2,3 +2,12 @@
 # https://thotismedia.com/annuaire-parcoursup-but-science-des-donees
 # https://thotismedia.com/annuaire-parcoursup-but-reseaux-et-telecommunications/#:~:text=Le%20BUT%20Réseaux%20et%20Télécommunications&text=Les%20matières%20abordées%20au%20cours,Télécommunications%20fixes%20et%20mobiles%2C%20etc.
 # https://thotismedia.com/annuaire-parcoursup-but-informatique
+
+
+# Autre site avec des infos grace a l'insep: 
+
+https://www.onisep.fr/ressources/univers-formation/formations/post-bac/but-reseaux-et-telecommunications-parcours-reseaux-operateurs-et-multimedia (RT)
+https://www.onisep.fr/ressources/univers-formation/formations/post-bac/but-science-des-donnees-parcours-exploration-et-modelisation-statistique (SD EMS)
+https://www.onisep.fr/ressources/univers-formation/formations/post-bac/but-science-des-donnees-parcours-visualisation-conception-d-outils-decisionnels (SD VCOD)
+https://www.onisep.fr/ressources/univers-formation/formations/post-bac/but-metiers-du-multimedia-et-de-l-internet-parcours-creation-numerique (MMI)
+https://www.onisep.fr/ressources/univers-formation/formations/post-bac/but-informatique-parcours-administration-gestion-et-exploitation-des-donnees (INFO)
