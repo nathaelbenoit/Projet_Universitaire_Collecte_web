5efcd8724c770ca0019c06cd1cd6742afb1c0efe
Benoit Nathaël <benoitnathael@gmail.com>
2025-11-06 11:26:37 +0100

    sites

---
+++ WS/ws.py
@@ -0,0 +1,4 @@
+# https://thotismedia.com/annuaire-parcoursup-but-mmi
+# https://thotismedia.com/annuaire-parcoursup-but-science-des-donees
+# https://thotismedia.com/annuaire-parcoursup-but-reseaux-et-telecommunications/#:~:text=Le%20BUT%20Réseaux%20et%20Télécommunications&text=Les%20matières%20abordées%20au%20cours,Télécommunications%20fixes%20et%20mobiles%2C%20etc.
+# https://thotismedia.com/annuaire-parcoursup-but-informatique
