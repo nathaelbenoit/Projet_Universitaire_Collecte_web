c6784ae048f1d18860451359026fc2fd653957b6
Nat2ume <benoitnathael@gmail.com>
2025-11-25 21:03:55 +0100

    Add PDF files and update API data fetching logic
    Added two PDF documents: 3-SujetSaée_FA.pdf and API/2-API.pdf. Updated API/api.py to fetch all records in batches of 100 and aggregate them into a DataFrame. Removed the unused programs_locations.csv file.

--- API/api.py
+++ API/api.py
@@ -3,16 +3,17 @@ import requests
 import pandas as pd
 
 url_base = "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/donnees-synop-essentielles-omm/records"
-
+    
 contenu = requests.get(url_base).json()
+print(contenu["total_count"])
+resultat_final = []
 
-df = pd.json_normalize(contenu['results'])
+for i in range(0, 10000, 100):
+    temp = requests.get(url_base + "?limit=100&offset=" + str(i)).json()
+    resultat_final += temp["results"]
+print(len(resultat_final))
 
-# Carte Folium interactive
-# Afficher la température moyenne par commune avec des marqueurs Folium :
-# ▪ couleur = en fonction de classe de température
-# ▪ info-bulle : nom de la commune et température moyenne.
-
+data = pd.DataFrame(resultat_final)
 
 
 # %%
